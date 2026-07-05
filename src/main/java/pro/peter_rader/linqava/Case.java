dedb132d9d92fee25b673b3d92b9db977a85dfe4
peter <admin@e-nexus.de>
2026-07-05 10:52:41 +0200

    Small fixes

--- src/main/java/pro/peter_rader/linqava/Case.java
+++ src/main/java/pro/peter_rader/linqava/Case.java
@@ -9,7 +9,6 @@ package pro.peter_rader.linqava;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * Fluent, compile-time-safe {@code CASE WHEN ... THEN ... [ELSE ...] END} expression builder.
